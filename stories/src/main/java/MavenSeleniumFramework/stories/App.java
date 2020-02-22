c86a3b40b9d156807bcb3312667b1b99e25be32b
Narasimha <Narasimha@192.168.4.8>
2020-02-22 11:47:47 +0530

    addedone

---
+++ stories/src/main/java/MavenSeleniumFramework/stories/App.java
@@ -0,0 +1,13 @@
+package MavenSeleniumFramework.stories;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
